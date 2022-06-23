c20536e757ffc70819efd80e52101bd97a849d45
dev <alejo1garciasosa@gmail.com>
2022-06-23 10:44:25 -0500

    PAGINATION READY

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -3,50 +3,38 @@ import VideogameCard from "../VideogameCard/VideogameCard.jsx";
 import NavBar from "../NavBar/NavBar.jsx";
 import { getVideogames } from "../../redux/actions/actions.js";
 import { useSelector, useDispatch } from "react-redux";
+import Pagination from "../Pagination/Pagination.jsx";
 
 const Home = () => {
   const dispatch = useDispatch();
   const videogames = useSelector((state) => state.videogames);
 
-  const [loading, setLoading] = useState(false);
+  //pagination
   const [currentPage, setCurrentPage] = useState(1);
-  const [postPerPage, setPostPerPage] = useState(5);
+  const [dataPerPage, setdataPerPage] = useState(5);
+  //get current data
+  const indexOfLastData = currentPage * dataPerPage;
+  const indexOfFirstData = indexOfLastData - dataPerPage;
+  const currentData = videogames.slice(indexOfFirstData, indexOfLastData);
+  const totalData = videogames.length;
 
   const getData = () => {
     return dispatch(getVideogames());
   };
   useEffect(() => {
-    setLoading(true);
     getData();
-    setLoading(false);
   }, []);
 
-  //get current post
-  const indexOfLastPost = currentPage * postPerPage;
-  const indexOfFirstPost = indexOfLastPost - postPerPage;
-  const currentPosts = videogames.slice(indexOfFirstPost, indexOfLastPost);
-
-  //pagination
-  const totalPost = videogames.length;
-  const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(totalPost / postPerPage); i++) {
-    pageNumbers.push(i);
-  }
-  const paginate = (number) => {
-    setCurrentPage(number);
-  };
   return (
     <div>
       <NavBar />
-      <nav>
-        {pageNumbers.map((number) => (
-          <li key={number}>
-            <ul onClick={() => paginate(number)}>{number}</ul>
-          </li>
-        ))}
-      </nav>
-      {currentPosts &&
-        currentPosts.map((videogame) => {
+      <Pagination
+        totalData={totalData}
+        dataPerPage={dataPerPage}
+        setCurrentPage={setCurrentPage}
+      />
+      {currentData &&
+        currentData.map((videogame) => {
           return (
             <VideogameCard
               key={videogame.id}
--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const Pagination = () => {
+const Pagination = ({ totalData, dataPerPage, setCurrentPage }) => {
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(totalData / dataPerPage); i++) {
+    pageNumbers.push(i);
+  }
+  const paginate = (number) => {
+    setCurrentPage(number);
+  };
 
-}
+  return (
+    <nav>
+      {pageNumbers.map((number) => (
+        <li key={number}>
+          <ul onClick={() => paginate(number)}>{number}</ul>
+        </li>
+      ))}
+    </nav>
+  );
+};
 
-export default Pagination
+export default Pagination;
