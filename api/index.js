1bb2344dceab709b59232619d59a424998f82b19
alejandrogh7 <alejo1garciasosa@gmail.com>
2022-07-04 16:53:12 -0500

    APP

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const { conn } = require("./src/db.js");
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   console.log("%s db updated");
-  server.listen(process.env.PORT, () => {
+  server.listen(process.env.PORT || 3001, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
